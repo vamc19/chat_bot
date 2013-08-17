a4d415f7b11beb774504b873b96afaf0c5cbeee1
Vamsi Krishna Atluri <vamc19@gmail.com>
2013-08-17 14:20:16 +0530

    Added PyAIML directory

--- chatBot.py
+++ chatBot.py
@@ -4,23 +4,29 @@
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 2 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       The AIML files used in this program can be downloaded for free from
 #                   http://aitools.org/Free_AIML_sets
+#
 
 
 from  PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
-import aiml
+
+try:
+    import aiml
+except ImportError:
+    print "Error importing PyAIML module."
+    exit()
 
 class chatBot(QDialog):
-    
+
     def __init__(self, parent = None):
         super(chatBot, self).__init__(parent)
         self.browser = QTextBrowser()
@@ -34,7 +40,7 @@ class chatBot(QDialog):
         self.connect(self.infield, SIGNAL("returnPressed()"), self.updateUi)
         self.setWindowTitle("Chat Bot")
         self.resize(400, 500)
-        
+
     def updateUi(self):
         text = unicode(self.infield.text())
         textString = QString("<b>Me</b> >> %s" %text)
@@ -43,13 +49,13 @@ class chatBot(QDialog):
         resultString = QString("<b>Bot</b> >> %s" %result)
         self.browser.append(resultString)
         self.infield.clear()
-        
+
 def main():
     app = QApplication(sys.argv)
     bot = chatBot()
     bot.show()
     app.exec_()
-    
+
 if __name__ == '__main__':
     brain = aiml.Kernel()
     brain.learn('brain/std-startup.xml')
